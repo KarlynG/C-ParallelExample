bce0fa46c1702c57bfa5c8a88f7fb93d6fe2312b
Karlyn Garcia Rojas <karlyn.garcia@solvex.com.do>
2022-04-21 14:29:53 -0400

    initial commit

---
+++ ParallelTest/FirstTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelTest
+{
+    internal class FirstTest
+    {
+        static void PizzaTask()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int totalPizza = 5;
+            Console.WriteLine($"Started preparing {totalPizza} pizza");
+            for (var x = 1; x <= totalPizza; x++)
+            {
+                MakePizza(x);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Finished preparing {totalPizza} pizza");
+            Console.WriteLine("Elapsed time:" + stopwatch.Elapsed.TotalSeconds);
+        }
+
+        static async Task PizzaTaskAsync()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int totalPizza = 5;
+            Console.WriteLine($"Started preparing {totalPizza} pizza");
+            var tasks = Enumerable.Range(1, totalPizza).Select(i => MakePizzaAsync(i));
+            await Task.WhenAll(tasks);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Finished preparing {totalPizza} pizza");
+            Console.WriteLine("Elapsed time:" + stopwatch.Elapsed.TotalSeconds);
+        }
+
+        static void MakePizza(int n)
+        {
+            PreparePizza(n);
+            BakePizza(n);
+        }
+
+        static async Task MakePizzaAsync(int n)
+        {
+            await PreparePizzaAsync(n);
+            await BakePizzaAsync(n);
+        }
+
+        static void PreparePizza(int n)
+        {
+            Console.WriteLine("Start preparing pizza " + n);
+            Thread.Sleep(1000);
+            Console.WriteLine("Finished preparing pizza " + n);
+        }
+
+        static void BakePizza(int n)
+        {
+            Console.WriteLine("Start baking pizza " + n);
+            Thread.Sleep(2000);
+            Console.WriteLine("Finished baking pizza " + n);
+        }
+        static async Task BakePizzaAsync(int n)
+        {
+            Console.WriteLine("Start baking pizza " + n);
+            await Task.Delay(2000);
+            Console.WriteLine("Finished baking pizza " + n);
+        }
+
+        static async Task PreparePizzaAsync(int n)
+        {
+            Console.WriteLine("Start preparing pizza " + n);
+            await Task.Delay(1000);
+            Console.WriteLine("Finished preparing pizza " + n);
+        }
+    }
+}
---
+++ ParallelTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelTest
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient();
+        public static int NumberOfIterations { get; } = 10;
+        static void Main(string[] args)
+        {
+            CallPokemonsNotParallel().GetAwaiter().GetResult();
+            Console.WriteLine("\n\n\n");
+            CallPokemonsAsync().GetAwaiter().GetResult();
+        }
+        private static async Task CallPokemonsNotParallel()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 1; i < NumberOfIterations; i++)
+            {
+                var result = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + i);
+                var response = JsonSerializer.Deserialize<Pokemon>(await result.Content.ReadAsStringAsync());
+                Console.WriteLine($"Pokemon #{i}: {response.name}");
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time:" + stopwatch.Elapsed.TotalSeconds);
+        }
+        private static async Task CallPokemonsAsync()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tasks = Enumerable.Range(1, NumberOfIterations).Select(i => CallPokemons(i));
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time:" + stopwatch.Elapsed.TotalSeconds);
+        }
+        private static async Task CallPokemons(int i)
+        {
+            var result = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + i);
+            var response = JsonSerializer.Deserialize<Pokemon>(await result.Content.ReadAsStringAsync());
+            Console.WriteLine($"Pokemon #{i}: {response.name}");
+        }
+    }
+    class Pokemon
+    {
+        public string name { get; set; }
+    }
+}
